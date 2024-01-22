fface6269a75a1255adb0d4185c23961ec704783
vmvangils <151744087+vmvangils@users.noreply.github.com>
2024-01-22 21:20:25 +0100

    Push1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon</title>
+    <link rel="stylesheet" href="style.css">
+    <?php require "nav.php" ?>
+
+</head>
+<h1 class="headtext">Pokémons</h1>
+
+
+<body>
+
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <div class="dropdown">
+        <button class="dropbtn">Pokémon</button>
+        <div class="dropdown-content">
+            <a href="Pokemons.php">Pokémon</a>
+            <a href="index.php">Homepage</a>
+
+        </div>
+    </div>
+</head>
+
+<body>
+
+</body>
+
+</html>
